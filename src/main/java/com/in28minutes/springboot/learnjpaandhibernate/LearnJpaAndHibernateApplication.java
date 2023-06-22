bed66ddf8d6c5da5f9ed8084fe3b2eb709540cf9
ameeraazli <wanazliwanameera@gmail.com>
2023-06-22 10:38:27 +0800

    add spotless

--- src/main/java/com/in28minutes/springboot/learnjpaandhibernate/LearnJpaAndHibernateApplication.java
+++ src/main/java/com/in28minutes/springboot/learnjpaandhibernate/LearnJpaAndHibernateApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LearnJpaAndHibernateApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LearnJpaAndHibernateApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(LearnJpaAndHibernateApplication.class, args);
+    }
 }
