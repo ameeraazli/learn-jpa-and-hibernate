5479e67e966ebc7bb4a6b43974441a904c64c4a6
ameeraazli <wanazliwanameera@gmail.com>
2023-06-22 17:14:42 +0800

    sample implemention of using Spring Data JPA (the previous one is just JPA) - this is even simpler and lesser code

--- src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/CourseCommandLineRunner.java
+++ src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/CourseCommandLineRunner.java
@@ -1,6 +1,6 @@
 package com.in28minutes.springboot.learnjpaandhibernate.course;
 
-import com.in28minutes.springboot.learnjpaandhibernate.course.jpa.CourseJpaRepository;
+import com.in28minutes.springboot.learnjpaandhibernate.course.springdatajpa.CourseSpringDataJpaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -8,16 +8,25 @@ import org.springframework.stereotype.Component;
 @Component
 public class CourseCommandLineRunner implements CommandLineRunner {
     //    @Autowired private CourseJdbcRepository repository;
-    @Autowired private CourseJpaRepository repository;
+    //    @Autowired private CourseJpaRepository repository;
+    @Autowired private CourseSpringDataJpaRepository repository;
 
     @Override
     public void run(String... args) throws Exception {
-        repository.insert(new Course(1, "Learn AWS JPA!", "in28minutes"));
-        repository.insert(new Course(2, "Learn Azure JPA!", "in28minutes"));
-        repository.insert(new Course(3, "Learn DevOps JPA!", "in28minutes"));
+        //        repository.insert(new Course(1, "Learn AWS JPA!", "in28minutes"));
+        //        repository.insert(new Course(2, "Learn Azure JPA!", "in28minutes"));
+        //        repository.insert(new Course(3, "Learn DevOps JPA!", "in28minutes"));
 
-        repository.deleteById(1);
+        repository.save(new Course(1, "Learn AWS SpringJPA!", "in28minutes"));
+        repository.save(new Course(2, "Learn Azure SpringJPA!", "in28minutes"));
+        repository.save(new Course(3, "Learn DevOps SpringJPA!", "in28minutes"));
 
-        System.out.println(repository.findById(2));
+        //        repository.deleteById(1);
+        //
+        //        System.out.println(repository.findById(2));
+
+        repository.deleteById(1l);
+
+        System.out.println(repository.findById(2l));
     }
 }
